a54e867d5a0abd5bcf822de7c004f46ad9a2e63b
Jay Lyerly <jay@oakcity.io>
2022-05-19 16:23:42 -0400

    0.1.29 - D1 update

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Python",
-            url: "https://callisto-xcframeworks.s3.amazonaws.com/mac/0.1.26/Python.xcframework.zip",
-            checksum: "60d504f3079db2e7100db385922d817f7faeb80957fd23159644438c866ded95"
+            url: "https://callisto-xcframeworks.s3.amazonaws.com/mac/0.1.29/Python.xcframework.zip",
+            checksum: "49f00f2c5fc2909b05ca9d7114dd92aa46002004a97701c59ff8ab13992cc708"
         )
         
     ]
