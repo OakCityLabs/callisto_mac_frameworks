9a50baf5b6052f09e4fdd7e8e81b83ed6f6a4ee0
Jay Lyerly <jay@oakcity.io>
2022-08-17 13:56:02 -0400

    Update D1 & callisto-python

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .binaryTarget(
             name: "Python",
             url: "https://callisto-xcframeworks.s3.amazonaws.com/mac/0.1.33/Python.xcframework.zip",
-            checksum: "7588ab696b660031dc23a9508edcc13b6f637740e452d12bff91679751e57d56"
+            checksum: "605444f9c0ffef1a37531c17eaec2c1a909e575e6af72b8da4c8f644dd1700af"
         )
         
     ]
